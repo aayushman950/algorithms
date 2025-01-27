65f8d9b7bff2fd5d809316acf63d6ed4e7f34fee
iusss <aayushman950@gmail.com>
2025-01-27 10:43:19 +0545

    add bottom up fibonacci implementation

---
+++ dynamic-programming/fibonacci-numbers/bottomup-fibonacci.py
@@ -0,0 +1,18 @@
+# Bottom-up dynamic programming without recursion
+def fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+        return []
+    elif n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
+    
+    FibArray = [0, 1]
+    for i in range(2, n):
+        FibArray.append(FibArray[i-1] + FibArray[i-2])
+    return FibArray
+
+# Driver Program
+n = int(input("Enter the value of n: "))
+print(fibonacci(n))
